0aa107de20582334d8fcfdbba82b8fb8cc6d4da6
Tristan Teufel <tristantristan@googlemail.com>
2023-08-18 10:17:18 +0200

    Update Projects

--- src/components/cv.tsx
+++ src/components/cv.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Link, Typography } from "@mui/material";
 import { useMdx } from "../hooks/useMdx";
 import { Layout } from "./layout";
 import { ProfileHeader } from "./profileHeader";
@@ -27,6 +27,19 @@ export const CV = () => {
         </Box>
         <Box>
           <OpenSource />
+        </Box>
+        <Box display="flex" justifyContent={"center"}>
+          <Typography variant="caption">
+            This CV is made with Gatsby, React, MUI, MDX and GraphQL check out
+            the{" "}
+            <Link
+              referrerPolicy="no-referrer"
+              target="_blank"
+              href="https://github.com/firsttris/gatsby-cv"
+            >
+              Source Code
+            </Link>
+          </Typography>
         </Box>
       </Container>
     </Layout>
