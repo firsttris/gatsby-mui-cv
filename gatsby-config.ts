be3524bd20a54d01d5783d360c57485742c7bd16
Tristan Teufel <tristantristan@googlemail.com>
2023-09-12 08:38:22 +0200

    add cname and homepage url

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,17 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: "/gatsby-mui-cv",
   siteMetadata: {
-    title: `gatsby-mui-cv`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "Tristan Teufel CV",
+    siteUrl: `https://cv.teufel-it.de`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    "gatsby-plugin-cname",
     "gatsby-plugin-emotion",
     "gatsby-plugin-image",
     "gatsby-plugin-sitemap",
