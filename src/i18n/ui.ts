9c1af713b05ec904c1e329360ea2a44a8c73e2ab
Tristan Teufel <tristanteufel@googlemail.com>
2023-11-19 06:37:01 +0100

    update cv

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -3,7 +3,7 @@ export const languages = {
     de: 'German',
 };
 
-export const defaultLang = navigator.language.includes('de') ? 'de' : 'en'; 
+export const defaultLang = (() => typeof window !== "undefined" && navigator.language.includes('de') ? 'de' : 'en')(); 
 
 export const ui = {
     en: {
