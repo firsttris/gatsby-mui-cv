3c566063189a04faba5f34eb4a286393caebaffc
Tristan Teufel <tristanteufel@googlemail.com>
2023-11-21 16:08:58 +0100

    fix missing key

--- src/components/ChipsWithIcon.tsx
+++ src/components/ChipsWithIcon.tsx
@@ -18,28 +18,28 @@ export const ChipsWithIcon: React.FC<Props> = ({ skills, ...otherProps }) => {
   return (
     <Box {...otherProps}>
       {skills.map((skill, index) => (
-        <Link href={skill.website} target="_blank" rel="noopener">
-                <Chip
-                onClick={() => console.log(skill.website)}
-          key={index}
-          label={
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '5px' }}>
-              <Icon icon={skill.icon} style={{ fontSize: '16px' }} />
-              <Typography>{skill.name}</Typography>
-            </Box>}
-          sx={{
-            cursor: 'pointer',
-            mx: "2px",
-            my: "3px",
-            height: "20px",
-            backgroundColor: "white",
-            border: "1px solid black",
-            borderRadius: "6px",
-            '& .MuiChip-label': {
-              px: '8px'
-            }
-          }}
-        />
+        <Link key={index} href={skill.website} target="_blank" rel="noopener">
+          <Chip
+            onClick={() => console.log(skill.website)}
+
+            label={
+              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '5px' }}>
+                <Icon icon={skill.icon} style={{ fontSize: '16px' }} />
+                <Typography>{skill.name}</Typography>
+              </Box>}
+            sx={{
+              cursor: 'pointer',
+              mx: "2px",
+              my: "3px",
+              height: "20px",
+              backgroundColor: "white",
+              border: "1px solid black",
+              borderRadius: "6px",
+              '& .MuiChip-label': {
+                px: '8px'
+              }
+            }}
+          />
         </Link>
 
       ))}
